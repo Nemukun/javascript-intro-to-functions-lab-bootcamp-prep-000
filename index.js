c10a3b78b1012cdc7cb308d63456821c43d3438c
Azumi Callanan <azucm@hotmail.com>
2017-12-04 07:53:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function whisper(string){
   return string.toLowerCase();
 }
 
-logShout(string){
+function logShout(string){
   console.log(string.toUpperCase());
 }
 
-logWhisper(string){
+function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
