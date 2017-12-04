9bdddf0e430230746ca190c1af1d5626628ac01c
Azumi Callanan <azucm@hotmail.com>
2017-12-04 07:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string){
   return string.toLowerCase();
 }
 
-
+logShout(string){
+  console.log(string.toUpperCase());
+}
 
 2) logShout(string) calls console.log() its one argument in all caps:
+
+3) logWhisper(string) calls console.log() its one argument in all lowercase:
